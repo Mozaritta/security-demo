b5405cef2c9dce9dc222602ece7741e7d984bf3b
Mozaritta <mozaritta@gmail.com>
2021-07-15 23:57:25 +0100

    Config file

--- src/main/java/com/security/demo/config/SecurityConfig.java
+++ src/main/java/com/security/demo/config/SecurityConfig.java
@@ -1,4 +1,26 @@
 package com.security.demo.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    protected AuthenticationManager authenticationManager() throws Exception{
+//        return super.authenticationManager();
+//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeRequests().anyRequest().permitAll();
+    }
 }
