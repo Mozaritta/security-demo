463b4b341f9d5f32fbf8a73f7c4e3755865fd487
Mozaritta <mozaritta@gmail.com>
2021-07-16 18:07:00 +0100

    sonfig add user authentication using UserService

--- src/main/java/com/security/demo/config/SecurityConfig.java
+++ src/main/java/com/security/demo/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.security.demo.config;
 
+import com.security.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,9 +17,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    protected AuthenticationManager authenticationManager() throws Exception{
 //        return super.authenticationManager();
 //    }
+    @Autowired
+    UserService userService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //In-memory
+//        auth.inMemoryAuthentication().withUser("Leila").password(passwordEncoder.encode("123")).authorities("USER", "ADMIN");
+
+        //Database Auth
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
         super.configure(auth);
     } 
 
@@ -24,6 +35,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // lo login and then have access to the main welcome page :
         httpSecurity.authorizeRequests().anyRequest().authenticated();
         httpSecurity.formLogin();
+    }
 
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/security/demo/service/UserService.java
+++ src/main/java/com/security/demo/service/UserService.java
@@ -1,12 +1,26 @@
 package com.security.demo.service;
 
+import com.security.demo.entity.User;
+import com.security.demo.repository.UserDetailsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserDetailsRepository userDetailsRepository;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        User user = userDetailsRepository.findByUserName(s);
+        if (user == null){
+            throw new UsernameNotFoundException("User not found with username " + s);
+        } else {
+            return user;
+        }
     }
 }
